e80e32440fff8eff91f97d990dd17859d6718efe
SyedRaihanuzzaman <142418984+SyedRaihanuzzaman@users.noreply.github.com>
2024-09-10 23:00:46 +0600

    Basic Coding is done.Now we can add note

--- app/src/main/java/com/example/yourname/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/example/yourname/sharedpreference/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.yourname.sharedpreference
 
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +22,26 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+        sharedPreferences = getSharedPreferences("NoteData",Context.MODE_PRIVATE)
+
+        binding.saveNoteBtn.setOnClickListener {
+            val note = binding.noteEt.text.toString()
+
+            val sharedEdit = sharedPreferences.edit()
+            sharedEdit.putString("note",note)
+            sharedEdit.apply()
+            Toast.makeText(this, "Note Added Successfully", Toast.LENGTH_SHORT).show()
+            binding.noteEt.text.clear()
+        }
+
+
+        binding.displayNoteBtn.setOnClickListener {
+
+            val storedNote = sharedPreferences.getString("note","")
+            binding.noteTV.text = "$storedNote"
+        }
 
     }
 }
