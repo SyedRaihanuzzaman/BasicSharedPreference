038432c1a0e921110e140de31316f8780c561e9b
SyedRaihanuzzaman <142418984+SyedRaihanuzzaman@users.noreply.github.com>
2024-09-10 22:46:21 +0600

    Design Part is done

---
+++ app/src/main/java/com/example/yourname/sharedpreference/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.yourname.sharedpreference
+
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.yourname.sharedpreference.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
